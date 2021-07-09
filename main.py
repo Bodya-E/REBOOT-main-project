5c5601ec58c274dcdfbb438fc8ac7af47def5142
Bodya-E <81853467+Bodya-E@users.noreply.github.com>
2021-07-09 04:55:20 +0300

    добавлен возраст

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 from magicsim import main_calculation_func
 
 app = FastAPI()
-
+'''
+демо-функция
 @app.get("/")
 def hello(text: Optional[str] = None):
     return "It works!"
-
+'''
 
 @app.get("/recommendations/")
-def make_some_recommendations(text: Optional[str] = None):
-    return main_calculation_func(text)
+def make_some_recommendations(rows: Optional[int] = 10, vacancy_age: Optional[int] = 120, text: Optional[str] = None):
+    """Запускает осовную функцию подбора"""
+    return main_calculation_func(text, rows, vacancy_age)
